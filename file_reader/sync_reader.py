b8f2cda8c5faccb0e8a48fd793a657f643c8494b
Артем Ковалев <159487645+Qusar0@users.noreply.github.com>
2024-07-22 19:46:57 +0300

    Update sync_reader.py

--- file_reader/sync_reader.py
+++ file_reader/sync_reader.py
@@ -1,18 +1,36 @@
 import datetime
 import time
+import os
 
 def read_file(filename):
     with open(filename, mode='rb') as f:
-        print(filename)
+        print("\n######start######", filename, sep="\n")
         while True:
             time.sleep(0.001)
             data = f.read(1024)
             if not data:
-                print(filename, "done ###############")
+                print(filename, "#######done######", sep="\n")
                 break
+
+def generator_helper(address, names):
+    for name in names:
+        if name.endswith(".bin"):
+            yield os.path.join(address, name)
+
+def generator(path):
+    for address, dirs, files in os.walk(path):
+        yield from generator_helper(address, files)
+
+
 def main():
+    # /home/user/sia/tmp/
+    path = input("Enter path to binary files: ")
     begin = datetime.datetime.now()
-    files = ['/home/sia/tmp/{}'.format(i) for i in range(1, 11)]
+    files = [item for item in generator(path)]
+
+    if len(files) == 0:
+        raise ValueError("No binary files in {}".format(path))
+
     for f in files:
         read_file(f)
 
